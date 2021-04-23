9857cfbe16443c0337470cee7b8c97ac1a87cb41
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-04-23 18:21:54 -0400

    Build out additional actions and reducers

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,6 +1,10 @@
 import {
     START_FETCHING,
     FETCHING_USER_SUCCESS,
+    UPDATE_USER_SUCCESS,
+    CREATE_PLANT_SUCCESS,
+    UPDATE_PLANT_SUCCESS,
+    DELETE_PLANT_SUCCESS,
     FETCH_ERROR
 } from '../actions';
 
@@ -21,6 +25,27 @@ export const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 user: action.payload,
+                isLoading: false
+            }
+        case UPDATE_USER_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                isLoading: false
+            }
+        case CREATE_PLANT_SUCCESS:
+            return {
+                ...state,
+                isLoading: false
+            }
+        case UPDATE_PLANT_SUCCESS:
+            return {
+                ...state,
+                isLoading: false
+            }
+        case DELETE_PLANT_SUCCESS:
+            return {
+                ...state,
                 isLoading: false
             }
         case FETCH_ERROR:
