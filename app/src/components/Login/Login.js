a20a083f08fe736dbea54d0df00623fe0f2c7778
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-04-30 17:24:16 -0400

    Login fixes

--- app/src/components/Login/Login.js
+++ app/src/components/Login/Login.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
 import { connect } from 'react-redux';
 import { login, clearError } from '../../actions';
 
-import Box from '@material-ui/core/Box';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const initialLoginCredentials = {
     username: '',
@@ -60,7 +58,7 @@ const Login = (props) => {
         },
         avatar: {
           margin: theme.spacing(1),
-          backgroundColor: theme.palette.secondary.main,
+          backgroundColor: theme.palette.primary.dark,
         },
         form: {
           width: '100%', // Fix IE 11 issue.
@@ -69,6 +67,12 @@ const Login = (props) => {
         submit: {
           margin: theme.spacing(3, 0, 2),
         },
+        signup: {
+          backgroundColor: "#C8E6C9",
+          "&:hover": {
+			backgroundColor: theme.palette.primary,
+		  }
+        }
       }));
 
       const classes = useStyles();
@@ -81,34 +85,39 @@ const Login = (props) => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Welcome, Login 
+            Login 
           </Typography>
             <form className={classes.form} onSubmit={handleSubmit} noValidate >
-                <TextField 
-                        type="text"
-                        name="username"
-                        value={loginCredentials.username}
-                        onChange={handleChange}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="userName"
-                        label="userName"
-                        autoFocus
-                    />
-                <TextField
-                        type="password"
-                        name="password"
-                        value={loginCredentials.password}
-                        onChange={handleChange}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="password"
-                        label="passsword"
-                        autoComplete="current-password"
-                    />
-                </label>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField 
+                                type="text"
+                                name="username"
+                                value={loginCredentials.username}
+                                onChange={handleChange}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="userName"
+                                label="Username"
+                                autoFocus
+                            />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                                type="password"
+                                name="password"
+                                value={loginCredentials.password}
+                                onChange={handleChange}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="password"
+                                label="Password"
+                                autoComplete="current-password"
+                            />
+                    </Grid>
+                </Grid>
             <Button
             type="submit"
             fullWidth
@@ -120,9 +129,14 @@ const Login = (props) => {
           </Button>
             </form>
             <h2>or</h2>
-            <Link to="/signup">
-				<button>Sign Up</button>
-			</Link>
+            <RouterLink to="/signup">
+				<Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.signup}
+                >Sign Up</Button>
+			</RouterLink>
             {
                 isLoading &&
                 <Box
@@ -134,6 +148,7 @@ const Login = (props) => {
                 </Box>
             }
         </div>
+        </Container>
     );
 }
  
