720c660dc5a1091db17e98d0539284f2e1f8f257
Melissa <wolfaholic@users.noreply.github.com>
2021-04-29 23:47:10 -0700

    styling issues but complete?
    got styling in place. having troubles with getting npm start to work. So i cant actually see if it is working. will need someone else to test.

--- app/src/components/Login/Login.js
+++ app/src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { login, clearError } from '../../actions';
 
 import Box from '@material-ui/core/Box';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 const initialLoginCredentials = {
     username: '',
@@ -38,29 +51,73 @@ const Login = (props) => {
         login(loginCredentials);
     }
 
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+          marginTop: theme.spacing(8),
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+          width: '100%', // Fix IE 11 issue.
+          marginTop: theme.spacing(1),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+        },
+      }));
+
+      const classes = useStyles();
+
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username
-                    <input 
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome, Login 
+          </Typography>
+            <form className={classes.form} onSubmit={handleSubmit} noValidate >
+                <TextField 
                         type="text"
                         name="username"
                         value={loginCredentials.username}
                         onChange={handleChange}
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="userName"
+                        label="userName"
+                        autoFocus
                     />
-                </label>
-                <label>
-                    Password
-                    <input 
+                <TextField
                         type="password"
                         name="password"
                         value={loginCredentials.password}
                         onChange={handleChange}
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="password"
+                        label="passsword"
+                        autoComplete="current-password"
                     />
                 </label>
-                <button disabled={isLoading}>Login</button>
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
             </form>
             <h2>or</h2>
             <Link to="/signup">
